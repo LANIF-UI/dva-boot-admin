d5823133046dd2ab089bae13b7c4963ab885199c
weiq <6682220aabc@163.com>
2018-04-19 10:08:59 +0800

    publish

--- config-overrides.js
+++ config-overrides.js
@@ -14,8 +14,6 @@ module.exports = function override(config, env) {
   
   if (env === "development") {
     config = injectBabelPlugin(["dva-hmr"], config);
-  } else {
-    config.output.publicPath = './';
   }
 
   config = injectBabelPlugin('transform-decorators-legacy', config);
