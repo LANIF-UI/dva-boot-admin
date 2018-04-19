e425710ef785e50d2a4ac550fb793014b01690d7
weiq <6682220aabc@163.com>
2018-04-19 19:55:02 +0800

    fullscreen

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,6 +10,31 @@ class NavBar extends Component {
     theme: '', //'bg-dark',
   }
 
+  toggleFullScreen() {
+    if (!document.fullscreenElement &&
+        !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+      } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+      } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+      } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      }
+    }
+  }
+
   render() {
     const {fixed, theme, onCollapseLeftSide, collapsed, onExpandTopBar} = this.props;
 
@@ -42,7 +67,7 @@ class NavBar extends Component {
               <Icon type="wand" />
             </a>
           </li>
-          <li className="hidden-xs">
+          <li className="hidden-xs" onClick={this.toggleFullScreen}>
             <a className="request-fullscreen toggle-active">
               <Icon type="screen-full" />
             </a>
