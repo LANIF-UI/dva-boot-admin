d7006e886e8b53d9682592b8e92c5472896af8fc
weiq <6682220aabc@163.com>
2018-04-28 11:42:43 +0800

    panel

--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Icon from '../Icon';
 import cx from 'classnames';
+import CSSAnimate from '../CSSAnimate';
 import './style/index.less';
 
 /**
@@ -11,31 +12,108 @@ class Panel extends Component {
     prefix: 'antui-panel'
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: props.collapse || false,
+      expand: props.expand || false
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const st = {};
+    if ('collapse' in nextProps) {
+      st.collapse = true;
+    } else if ('expand' in nextProps) {
+      st.expand = true;
+    }
+    if (Object.keys(st).length) {
+      this.setState(st);
+    }
+  }
+
+  toggleExpandOrCollapse = open => e => {
+    const { onChange } = this.props;
+    this.setState({
+      expand: !!open,
+      collapse: !open
+    });
+    if (onChange) {
+      onChange({
+        expand: !!open,
+        collapse: !open
+      });
+    }
+  };
+
+  onExpand = expand => e => {
+    this.setState({
+      expand,
+      collapse: false,
+    });
+  }
+
+  onCollapse = collapse => e => {
+    this.setState({
+      collapse,
+      expand: false,
+    });
+  }
+
   render() {
-    const { theme, prefix, className, title, width, style } = this.props;
+    const { expand, collapse } = this.state;
+    const {
+      theme,
+      prefix,
+      className,
+      title,
+      width,
+      style,
+      children,
+      onRefresh,
+      onClose
+    } = this.props;
 
     const classnames = cx(prefix, className, {
-      theme: !!theme
+      theme: !!theme,
+      'panel-fullscreen': !!expand,
+      'panel-collapsed': !!collapse
     });
 
     const styles = {
       ...style,
-      width,
-    }
+      width
+    };
 
     return (
       <div className={classnames} style={styles}>
         <div className={`${prefix}-header`}>
           <span className={`${prefix}-header-title`}>{title}</span>
           <span className={`${prefix}-header-controls`}>
-            <a className="panel-control-loader"><Icon type="refresh" /></a>
-            <a className="panel-control-fullscreen"><Icon type="enlarge" /></a>
-            <a className="panel-control-mini"><Icon type="minus" /></a>
-            <a className="panel-control-remove"><Icon type="close" /></a>
+            <a className="panel-control-loader" onClick={onRefresh}>
+              <Icon type="refresh" />
+            </a>
+            <a
+              className="panel-control-fullscreen"
+              onClick={this.onExpand(expand ? true: false)}
+            >
+              <Icon type={`${expand ? 'enlarge' : 'shrink'}`} />
+            </a>
+            <a
+              className="panel-control-collapsed"
+              onClick={this.onCollapse(collapse ? true : false)}
+            >
+              <Icon type={`${collapse ? 'plus' : 'minus'}`} />
+            </a>
+            <a className="panel-control-remove" onClick={onClose}>
+              <Icon type="close" />
+            </a>
           </span>
         </div>
-        <div className={`${prefix}-panel-body`}>
-
+        <div className={`${prefix}-body`}>
+          <div className="panel-content">
+            {children}
+          </div>
         </div>
       </div>
     );
--- src/routes/Dashboard/components/index.js
+++ src/routes/Dashboard/components/index.js
@@ -4,13 +4,18 @@ import { Layout } from 'antd';
 import BaseComponent from 'components/BaseComponent';
 import Panel from 'components/Panel';
 import './index.less';
+const { Content } = Layout;
 
 @connect()
 export default class Dashboard extends BaseComponent {
   render() {
     return (
-      <Layout className="full-layout dashboard-page">
-        <Panel title="数据面板组件" width={300} />
+      <Layout className="full-layout page dashboard-page">
+        <Content>
+          <Panel title="数据面板组件" width={300}>
+            abcabacababc
+          </Panel>
+        </Content>
       </Layout>
     )
   }
