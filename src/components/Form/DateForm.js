fe184916af9db07ff4473f41704ff750b4c998c7
weiq <6682220aabc@163.com>
2018-09-17 15:18:47 +0800

    :cake: fix time~ type error

--- src/components/Form/DateForm.js
+++ src/components/Form/DateForm.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import {DatePicker, TimePicker} from 'antd';
-import moment from 'moment';
+import { DatePicker, TimePicker } from 'antd';
+import $$ from 'cmn-utils';
 const { MonthPicker, RangePicker } = DatePicker;
 /**
  * 日期，时间元件
  */
-export default ({name, form, type, record, initialValue, rules, formFieldOptions = {}, format, onChange, preview, ...otherProps}) => {
+export default ({
+  name,
+  form,
+  type,
+  record,
+  initialValue,
+  rules,
+  formFieldOptions = {},
+  format,
+  onChange,
+  normalize,
+  preview,
+  ...otherProps
+}) => {
   const { getFieldDecorator } = form;
-  
+
   let initval = initialValue;
-  
+
   if (record) {
     initval = record[name];
   }
-  
+
   // 如果存在初始值
-  if (initval !== null && typeof (initval) !== "undefined") {
-    formFieldOptions.initialValue = moment(initval);
+  if (initval !== null && typeof initval !== 'undefined') {
+    if ($$.isFunction(normalize)) {
+      formFieldOptions.initialValue = normalize(initval);
+    } else {
+      formFieldOptions.initialValue = initval;
+    }
   }
 
   // 如果有rules
@@ -28,7 +45,7 @@ export default ({name, form, type, record, initialValue, rules, formFieldOptions
 
   let props = {
     ...otherProps
-  }
+  };
 
   switch (type) {
     case 'date':
@@ -52,21 +69,25 @@ export default ({name, form, type, record, initialValue, rules, formFieldOptions
   }
 
   // 如果需要onChange
-  if (typeof onChange === "function") {
-    formFieldOptions.onChange = (date, dateString) => onChange(form, date, dateString);
+  if (typeof onChange === 'function') {
+    formFieldOptions.onChange = (date, dateString) =>
+      onChange(form, date, dateString);
   }
 
   if (format) props.format = format;
-  else if (type === 'month') props.format = "YYYY-MM";
-  else if (type === 'datetime' || type === 'date~') props.format = "YYYY-MM-DD HH:mm:ss";
-  else if (type === 'time') props.format = "HH:mm:ss";
-  else props.format = "YYYY-MM-DD";
+  else if (type === 'month') props.format = 'YYYY-MM';
+  else if (type === 'datetime' || type === 'date~')
+    props.format = 'YYYY-MM-DD HH:mm:ss';
+  else if (type === 'time') props.format = 'HH:mm:ss';
+  else props.format = 'YYYY-MM-DD';
 
   if (preview) {
-    return <div style={otherProps.style}>{initval ? formFieldOptions.initialValue.format(props.format) : ''}</div>;
+    return (
+      <div style={otherProps.style}>
+        {initval ? formFieldOptions.initialValue.format(props.format) : ''}
+      </div>
+    );
   }
 
-  return getFieldDecorator(name, formFieldOptions)(
-    <Component {...props} />
-  );
+  return getFieldDecorator(name, formFieldOptions)(<Component {...props} />);
 };
