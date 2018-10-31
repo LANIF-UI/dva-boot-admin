7e3901f299f55fd6e9c634ddd91dba8bc34cf5a5
weiq <6682220aabc@163.com>
2018-10-31 09:53:32 +0800

    :cake: update TopBar.js

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -8,19 +8,44 @@ import Mask from '../Mask';
 import './style/index.less';
 
 class TopBar extends Component {
-  state = {
-    currentRoute: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentRoute: this.getRouteLevel(props.location.pathname) || []
+    };
   }
 
   componentWillReceiveProps(nextProps) {
-    this.getRouteLevel(nextProps.location.pathname);
+    const currentRoute = this.getRouteLevel(nextProps.location.pathname);
+
+    this.setState({
+      currentRoute
+    });
   }
 
+  getRouteLevel = pathName => {
+    const orderPaths = [];
+    pathName.split('/').reduce((prev, next) => {
+      const path = [prev, next].join('/');
+      orderPaths.push(path);
+      return path;
+    });
+
+    return orderPaths
+      .map(item => window.dva_router_pathMap[item])
+      .filter(item => !!item);
+  };
+
   render() {
-    const { expand, toggleRightSide, collapsedRightSide, onCollapse } = this.props;
+    const {
+      expand,
+      toggleRightSide,
+      collapsedRightSide,
+      onCollapse
+    } = this.props;
     const { currentRoute } = this.state;
     const classnames = cx('topbar', {
-      'topbar-expand': expand,
+      'topbar-expand': expand
     });
 
     return (
@@ -28,7 +53,10 @@ class TopBar extends Component {
         <div className="topbar-dropmenu">
           <Row gutter={22}>
             <Col xs={8} md={4}>
-              <CSSAnimate className="animated-short" type={expand ? "fadeInDown" : "fadeOutUp"}>
+              <CSSAnimate
+                className="animated-short"
+                type={expand ? 'fadeInDown' : 'fadeOutUp'}
+              >
                 <a className="metro-tile">
                   <Icon type="message" />
                   <span className="metro-title">信息</span>
@@ -36,7 +64,10 @@ class TopBar extends Component {
               </CSSAnimate>
             </Col>
             <Col xs={8} md={4}>
-              <CSSAnimate className="animated-short" type={expand ? "fadeInDown" : "fadeOutUp"}>
+              <CSSAnimate
+                className="animated-short"
+                type={expand ? 'fadeInDown' : 'fadeOutUp'}
+              >
                 <a className="metro-tile">
                   <Icon type="user" />
                   <span className="metro-title">用户</span>
@@ -44,15 +75,21 @@ class TopBar extends Component {
               </CSSAnimate>
             </Col>
             <Col xs={8} md={4}>
-              <CSSAnimate className="animated-short" type={expand ? "fadeInDown" : "fadeOutUp"}>
+              <CSSAnimate
+                className="animated-short"
+                type={expand ? 'fadeInDown' : 'fadeOutUp'}
+              >
                 <a className="metro-tile">
-                <Icon type="headphones" />
+                  <Icon type="headphones" />
                   <span className="metro-title">支持</span>
                 </a>
               </CSSAnimate>
             </Col>
             <Col xs={8} md={4}>
-              <CSSAnimate className="animated-short" type={expand ? "fadeInDown" : "fadeOutUp"}>
+              <CSSAnimate
+                className="animated-short"
+                type={expand ? 'fadeInDown' : 'fadeOutUp'}
+              >
                 <a className="metro-tile">
                   <Icon type="equalizer" />
                   <span className="metro-title">设置</span>
@@ -60,7 +97,10 @@ class TopBar extends Component {
               </CSSAnimate>
             </Col>
             <Col xs={8} md={4}>
-              <CSSAnimate className="animated-short" type={expand ? "fadeInDown" : "fadeOutUp"}>
+              <CSSAnimate
+                className="animated-short"
+                type={expand ? 'fadeInDown' : 'fadeOutUp'}
+              >
                 <a className="metro-tile">
                   <Icon type="play" />
                   <span className="metro-title">视频</span>
@@ -68,7 +108,10 @@ class TopBar extends Component {
               </CSSAnimate>
             </Col>
             <Col xs={8} md={4}>
-              <CSSAnimate className="animated-short" type={expand ? "fadeInDown" : "fadeOutUp"}>
+              <CSSAnimate
+                className="animated-short"
+                type={expand ? 'fadeInDown' : 'fadeOutUp'}
+              >
                 <a className="metro-tile">
                   <Icon type="image" />
                   <span className="metro-title">图片</span>
@@ -78,36 +121,42 @@ class TopBar extends Component {
           </Row>
         </div>
         <header className="topbar-content">
-          { currentRoute.length ? (
+          {currentRoute.length ? (
             <Breadcrumb>
-              <Breadcrumb.Item className="first">{currentRoute[currentRoute.length-1].title}</Breadcrumb.Item>
-              <Breadcrumb.Item className="icon"><Icon type="home" /></Breadcrumb.Item>
-              <Breadcrumb.Item><Link to="/">主页</Link></Breadcrumb.Item>
+              <Breadcrumb.Item className="first">
+                {currentRoute[currentRoute.length - 1].title}
+              </Breadcrumb.Item>
+              <Breadcrumb.Item className="icon">
+                <Icon type="home" />
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link to="/">主页</Link>
+              </Breadcrumb.Item>
               {currentRoute.map((item, index) => (
-                <Breadcrumb.Item key={index}>{ index === currentRoute.length-1 ? item.title : <Link to={item.path}>{item.title}</Link>}</Breadcrumb.Item>
+                <Breadcrumb.Item key={index}>
+                  {index === currentRoute.length - 1 ? (
+                    item.title
+                  ) : (
+                    <Link to={item.path}>{item.title}</Link>
+                  )}
+                </Breadcrumb.Item>
               ))}
             </Breadcrumb>
-          ) : null }
-          <a className={cx("topbar-right", {"collapse": collapsedRightSide})} onClick={toggleRightSide}>
+          ) : null}
+          <a
+            className={cx('topbar-right', { collapse: collapsedRightSide })}
+            onClick={toggleRightSide}
+          >
             <Icon type="into" />
           </a>
         </header>
-        <Mask visible={expand} onClose={onCollapse} getContainer={node => node.parentNode} />
+        <Mask
+          visible={expand}
+          onClose={onCollapse}
+          getContainer={node => node.parentNode}
+        />
       </div>
     );
-  }
-
-  getRouteLevel = (pathName) => {
-    const orderPaths = [];
-    pathName.split('/').reduce((prev, next) => {
-      const path = [prev, next].join('/');
-      orderPaths.push(path);
-      return path;
-    });
-
-    this.setState({
-      currentRoute: orderPaths.map(item => window.dva_router_pathMap[item]).filter(item => !!item)
-    });
   }
 }
 
