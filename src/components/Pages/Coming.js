6b522b3a8b64df30c562ef5afa68afd75c534978
weiq <6682220aabc@163.com>
2019-01-29 15:13:56 +0800

    :cake: Update Coming Page

--- src/components/Pages/Coming.js
+++ src/components/Pages/Coming.js
@@ -1,10 +1,79 @@
 import './style/coming.less';
 import React, { Component } from 'react';
 import { Layout } from 'antd';
+import moment from 'moment';
 const { Content } = Layout;
 
+/**
+ * 倒计时页面
+ * <Coming
+     title="精彩即将呈现"
+     value={Date.now() + 1000 * 60 * 60 * 24 * 2}
+   />
+ */
 class Coming extends Component {
+  state = {
+    days: '00',
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  };
+
+  countdownId = null;
+
+  componentDidMount() {
+    this.syncTimer();
+  }
+
+  componentDidUpdate() {
+    this.syncTimer();
+  }
+
+  componentWillUnmount() {
+    this.stopTimer();
+  }
+
+  syncTimer = () => {
+    const { value } = this.props;
+
+    const timestamp = moment(value).valueOf();
+    if (timestamp >= Date.now()) {
+      this.startTimer();
+    } else {
+      this.stopTimer();
+    }
+  };
+
+  startTimer = () => {
+    const { value } = this.props;
+
+    if (this.countdownId) return;
+
+    this.countdownId = window.setInterval(() => this.parseTime(value), 500);
+  };
+
+  stopTimer = () => {
+    clearInterval(this.countdownId);
+    this.countdownId = null;
+  };
+
+  parseTime = value => {
+    const target = moment(value);
+    const current = moment();
+    const diffTime = Math.max(target - current, 0);
+    const duration = moment.duration(diffTime, 'milliseconds');
+
+    this.setState({
+      days: formatDoubleStr(duration.days()),
+      hours: formatDoubleStr(duration.hours()),
+      minutes: formatDoubleStr(duration.minutes()),
+      seconds: formatDoubleStr(duration.seconds())
+    });
+  };
+
   render() {
+    const { title } = this.props;
+    const { days, hours, minutes, seconds } = this.state;
     return (
       <Layout className="full-layout coming-soon-page">
         <Content>
@@ -14,24 +83,22 @@ class Coming extends Component {
               <div className="saw" />
               <div className="balk" />
             </div>
-
-            <h3 className="title">精彩即将呈现</h3>
-
+            <div className="title">{title}</div>
             <div id="watch">
               <div className="dash days_dash">
-                <div className="digit">00</div>
+                <div className="digit">{days}</div>
                 <span className="dash_title">天</span>
               </div>
               <div className="dash hours_dash">
-                <div className="digit">00</div>
+                <div className="digit">{hours}</div>
                 <span className="dash_title">时</span>
               </div>
               <div className="dash minutes_dash">
-                <div className="digit">00</div>
+                <div className="digit">{minutes}</div>
                 <span className="dash_title">分</span>
               </div>
               <div className="dash seconds_dash">
-                <div className="digit">00</div>
+                <div className="digit">{seconds}</div>
                 <span className="dash_title">秒</span>
               </div>
             </div>
@@ -43,3 +110,7 @@ class Coming extends Component {
 }
 
 export default Coming;
+
+function formatDoubleStr(number) {
+  return number < 10 ? '0' + number : number;
+}
