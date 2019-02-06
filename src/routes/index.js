7442983da5da479d154988ccb00f3ce6c7712cb5
魏小雨 <6682220aabc@163.com>
2019-02-06 11:41:05 +0800

    :cake: add comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
-import {createRoutes} from '@/utils/core';
+import { createRoutes } from '@/utils/core';
 import BasicLayout from '@/layouts/BasicLayout';
-// import CardLayout from '@/layouts/CardLayout';
 import UserLayout from '@/layouts/UserLayout';
 import NotFound from './Pages/404';
 import ScreenLock from './Pages/ScreenLock';
@@ -32,7 +31,16 @@ import Alerts from './UI/Alerts';
 import Button from './UI/Button';
 import CRUD from './Business/CRUD';
 
-const routesConfig = (app) => ([
+/**
+ * 主路由配置
+ * 
+ * path 路由地址
+ * component 组件
+ * indexRoute 默认显示路由
+ * childRoutes 所有子路由
+ * NotFound 路由要放到最下面，当所有路由当没匹配到时会进入这个页面
+ */
+const routesConfig = app => [
   {
     path: '/sign',
     title: '登录',
@@ -41,9 +49,10 @@ const routesConfig = (app) => ([
     childRoutes: [
       Login(app),
       Register(app),
-      NotFound()  // 这个要放到最下面，当所有路由当没匹配到时会进入这个页面
+      NotFound()
     ]
-  }, {
+  },
+  {
     path: '/',
     title: '系统中心',
     component: BasicLayout,
@@ -75,9 +84,9 @@ const routesConfig = (app) => ([
       Print(),
       Banner(app),
       LevelRoute(app),
-      NotFound(), // 这个要放到最下面，当所有路由当没匹配到时会进入这个页面
+      NotFound()
     ]
   }
-]);
+];
 
 export default app => createRoutes(app, routesConfig);
