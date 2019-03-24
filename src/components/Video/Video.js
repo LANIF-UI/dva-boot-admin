ca8ef2b9a289819b2110c90349a00d0ae0638a1a
魏小雨 <6682220aabc@163.com>
2019-03-24 22:37:37 +0800

    :cake: add video 0%

---
+++ src/components/Video/Video.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './style/index.less';
+
+/**
+ * 视频播放组件
+ */
+class Video extends Component {
+  static defaultProps = {
+    prefixCls: 'antui-video'
+  };
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Video;
