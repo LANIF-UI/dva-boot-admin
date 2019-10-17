f8afac2c4a6eb9eef80aafbe05f2c3a67e797dc0
徐书迪 <deantsui0xff@gmail.com>
2019-10-17 21:57:00 +0800

    add children to pageLoading

--- src/components/Loading/PageLoading.js
+++ src/components/Loading/PageLoading.js
@@ -4,5 +4,9 @@ import './PageLoading.less'
 /**
  * 加载效果示例
  */
-export default ({loading, style = 'style1'}) => 
-  loading ? <div className={`loading-spinner loading-spinner-${style}`}></div> : null;
+export default class extends React.Component{
+  render(){
+    const {loading,style = 'style1'} = this.props
+    return   loading ? <div className={`loading-spinner loading-spinner-${style}`}></div> : this.props.children || null;
+  }
+}
